69debb26fa84f6327453301a08e5473864726b0b
cowboysj <su10jin11@khu.ac.kr>
2024-07-29 14:00:09 +0900

    :fire: [Refactor] Main.java 삭제, Application 코드 수정

--- src/main/java/com/simter/SimterApplication.java
+++ src/main/java/com/simter/SimterApplication.java
@@ -1,18 +1,13 @@
 package com.simter;
 
 import io.github.cdimascio.dotenv.Dotenv;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SimterApplication {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
         Dotenv dotenv = Dotenv.configure().load();
 
         // 환경 변수를 시스템 속성으로 설정
@@ -21,39 +16,5 @@ public class SimterApplication {
         System.setProperty("DB_USER", dotenv.get("DB_USER"));
         System.setProperty("DB_PW", dotenv.get("DB_PW"));
         SpringApplication.run(SimterApplication.class, args);
-	}
-
-    @Entity
-    @Getter
-    @Builder
-    @NoArgsConstructor(access = AccessLevel.PROTECTED)
-    @AllArgsConstructor
-    public static class Member {
-
-        @Id
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        private Long user_id;
-
-        private String password;
-
-        private String nickname;
-
-        private Boolean has_airplane = false;
-
-        private String chatbot;
-
-        private String email;
-
-        private String login_type;
-
-        private String status;
-
-        private String inactive_data;
-
-        private String latest_chatbot;
-
-        private Boolean mail_alert = false;
-
-
     }
 }
