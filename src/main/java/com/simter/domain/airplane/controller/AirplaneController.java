b3c2c348b1b6ccdf98cb83a1b749380fdd6d8f59
cowboysj <su10jin11@khu.ac.kr>
2024-08-08 01:34:20 +0900

    :sparkles: [Feat] Airplane 스웨거 주석 추가

--- src/main/java/com/simter/domain/airplane/controller/AirplaneController.java
+++ src/main/java/com/simter/domain/airplane/controller/AirplaneController.java
@@ -6,13 +6,17 @@ import com.simter.domain.airplane.dto.AirplanePostRequestDto;
 import com.simter.domain.airplane.dto.AirplanePostResponseDto;
 
 import com.simter.domain.airplane.service.AirplaneService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+@Tag(name = "종이비행기 API", description = "종이비행기를 작성하고 조회하는 API")
 @RestController
 @RequestMapping("/api/v1")
 @RequiredArgsConstructor
@@ -22,6 +26,7 @@ public class AirplaneController {
     private static final Logger logger = LoggerFactory.getLogger(AirplaneController.class);
 
     // 종이비행기 작성 API (POST)
+    @Operation(summary = "종이비행기 작성 API", description = "종이비행기를 작성하는 API")
     @PostMapping("/airplane")
     public ApiResponse<AirplanePostResponseDto> sendAirplane(@RequestBody AirplanePostRequestDto requestDto) {
         airplaneService.sendAirplane(requestDto);
@@ -29,6 +34,7 @@ public class AirplaneController {
     }
 
     // 종이비행기 조회 API (GET)
+    @Operation(summary = "종이비행기 조회 API", description = "종이비행기를 조회하는 API")
     @GetMapping("/airplane/{receiverId}")
     public ApiResponse<AirplaneGetResponseDto> getAirplane(@PathVariable Long receiverId) {
 
--- src/main/java/com/simter/domain/member/repository/MemberRepository.java
+++ src/main/java/com/simter/domain/member/repository/MemberRepository.java
@@ -1,6 +1,7 @@
 package com.simter.domain.member.repository;
 
 import com.simter.domain.member.entity.Member;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -8,5 +9,5 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 
     Optional<Member> findByEmail(String email);
 
-
+    List<Member> findAllByHasAirplane(Boolean hasAirplane);
 }
