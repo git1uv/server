b8810e7e4580a9692fd654c3934bb784febb0eee
Sujin Kim <su10jin11@khu.ac.kr>
2024-09-18 02:05:27 +0900

    :rocket: [!hotfix] Test 코드 삭제

--- src/test/java/com/simter/SimterApplicationTests.java
+++ src/test/java/com/simter/SimterApplicationTests.java
@@ -1,9 +1,13 @@
-package simter.simter;
+package com.simter;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SimterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
 
 }
