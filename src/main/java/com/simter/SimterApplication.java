816c59c270f3d8f77fd02a1d0ddbad166b5c6eb7
minahkim03 <minahkim026@gmail.com>
2024-09-25 18:28:11 +0900

    :bug: 오늘일기, 감정 default 수정

--- src/main/java/com/simter/SimterApplication.java
+++ src/main/java/com/simter/SimterApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ㅎㅑㅅSimterApplication {
+public class SimterApplication {
 
     public static void main(String[] args) {
 
