3d5f18b7d654e010e2cd70283e8daef2ac35ca2d
minahkim03 <minahkim026@gmail.com>
2024-10-03 00:16:02 +0900

    :bug: error handler JWT 토큰 예외 처리 추가

--- src/main/java/com/simter/apiPayload/exception/handler/ErrorHandler.java
+++ src/main/java/com/simter/apiPayload/exception/handler/ErrorHandler.java
@@ -5,7 +5,14 @@ import com.simter.apiPayload.exception.GeneralException;
 
 public class ErrorHandler extends GeneralException {
 
+    private final BaseCode baseCode;
+
     public ErrorHandler(BaseCode baseCode) {
         super(baseCode);
+        this.baseCode = baseCode;
+    }
+
+    public BaseCode getErrorStatus() {
+        return this.baseCode;
     }
 }
