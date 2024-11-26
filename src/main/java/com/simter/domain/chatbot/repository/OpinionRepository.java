0b225e3710251d6ca19e144991236878997b72fe
Sujin Kim <su10jin11@khu.ac.kr>
2024-11-26 17:23:20 +0900

    feat : 챗봇 개인화 프롬프트 수정

--- src/main/java/com/simter/domain/chatbot/repository/OpinionRepository.java
+++ src/main/java/com/simter/domain/chatbot/repository/OpinionRepository.java
@@ -1,8 +1,12 @@
 package com.simter.domain.chatbot.repository;
 
 import com.simter.domain.chatbot.entity.Opinion;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface OpinionRepository extends JpaRepository<Opinion, Long> {
-
+    @Query("SELECT o FROM Opinion o WHERE o.member.email = :email ORDER BY o.createdAt DESC")
+    List<Opinion> findTop3ByMemberEmailOrderByCreatedAtDesc(@Param("email") String email);
 }
